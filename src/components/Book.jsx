32d1c8ac1dee618639458542cf43bec3cd05e62f
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-28 00:23:21 +0700

    FIX : Responsive untuk mobile pada componets Book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -47,11 +47,11 @@ const Book = () => {
   };
 
   return (
-    <div className="max-w-5xl mt-10 mx-10  md:mx-auto p-6 bg-white rounded-lg shadow-md shadow-slate-500" id='Book'>
+    <div className="max-w-5xl mt-10 mx-4 md:mx-auto p-4 md:p-6 bg-white rounded-lg shadow-md shadow-slate-500" id='Book'>
       <h1 className="text-2xl font-bold mb-6">Book</h1>
 
       {/* Pilihan Perjalanan */}
-      <div className="flex space-x-4 mb-6">
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
         <label className="flex items-center">
           <input 
             type="radio" 
@@ -88,7 +88,7 @@ const Book = () => {
       </div>
 
       {/* Pilihan Keberangkatan */}
-      <div className="grid grid-cols-3 gap-4 mb-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         <div>
           <label className="block mb-2">Keberangkatan</label>
           <select 
@@ -208,9 +208,9 @@ const Book = () => {
       </div>
 
       {/* Promo Code */}
-      <div className="flex justify-between items-center mb-6">
-        <div className="flex items-center space-x-4 w-full">
-          <div className="flex items-center">
+      <div className="flex flex-col md:flex-row justify-between items-center mb-6 space-y-4 md:space-y-0">
+        <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 w-full">
+          <div className="flex items-center w-full md:w-auto">
             <svg 
               xmlns="http://www.w3.org/2000/svg" 
               className="h-5 w-5 mr-2 text-green-600" 
@@ -230,9 +230,9 @@ const Book = () => {
             value={promoCode}
             onChange={(e) => setPromoCode(e.target.value)}
             placeholder="Enter promo code" 
-            className="flex-grow p-2 border rounded"
+            className="flex-grow p-2 border rounded w-full"
           />
-          <button className="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700 transition-colors">
+          <button className="bg-green-600 text-white px-6 py-2 rounded hover:bg-green-700 transition-colors w-full md:w-auto mt-4 md:mt-0">
             Cari Keberangkatan
           </button>
         </div>
