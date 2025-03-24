e6769f1753462d806d4d1c8e1475b9e23a75a869
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-03-24 22:02:55 +0700

    [ADD] Mengimplementasikan Library react-router-dom untuk router pada website

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-dom/client';
 import App from './App.jsx'
+import path from 'path';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element : <App/>
+  },
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
