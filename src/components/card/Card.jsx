c8aa006eb89a4e13326b85b278bd07bc45811beb
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-03-24 22:16:24 +0700

    [ADD] Menambah file untuk component card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <p>ini halaman card</p>
+    </div>
+  )
+}
+
+export default Card
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
+import Card from './components/card/Card.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element : <App/>
   },
+  {
+    path: "/card",
+    element: <Card/>
+  }
 ])
 
 
